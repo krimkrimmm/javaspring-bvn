b041c82c3c241fad37029e6967d9fdeddce4aa73
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2025-04-15 22:58:32 +0700

    Add files via upload

---
+++ buoi25/src/test/java/vn/scrip/buoi25/Buoi25ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.scrip.buoi25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4148ad1f0fa3eb2e1490c5dc766573199fc4bfba
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2025-04-15 22:58:09 +0700

    Add files via upload

---
+++ src/main/java/vn/scrip/buoi25/Buoi25Application.java
@@ -0,0 +1,12 @@
+package vn.scrip.buoi25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Buoi25Application
+{
+	public static void main(String[] args) {
+		SpringApplication.run(Buoi25Application.class, args);
+	}
+
+}
